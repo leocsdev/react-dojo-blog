a8742793098d7a278ebb2a5c3232ab6fb11babde
Leo <leocsdev@gmail.com>
2021-02-27 18:04:02 +0800

    Made a comment about the name var being non-reactive.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 const Home = () => {
+  // name var is not reactive
+  let name = "Mario";
+
   const handleClick = () => {
-    console.log("I'm clicked!");
+    // name value changes to Luigi when the button below is clicked
+    name = "Luigi";
+    console.log(name);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+
+      {/* name will not update to Luigi, it stays with Mario */}
+      <p>{name}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
