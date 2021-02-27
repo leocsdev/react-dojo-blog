28e36a1bd5ae75fc98c9179d97a7248e81df2590
Leo <leocsdev@gmail.com>
2021-02-27 18:47:07 +0800

    Utilize useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
-  // name var is not reactive
-  let name = "Mario";
+  // // name var is not reactive
+  // let name = "Mario";
+
+  // Utilize useState instead to become reactive
+  const [name, setName] = useState("Mario");
 
   const handleClick = () => {
-    // name value changes to Luigi when the button below is clicked
-    name = "Luigi";
+    // // name value changes to Luigi when the button below is clicked
+    // name = "Luigi";
+    // console.log(name);
+
+    // Set name to Luigi once button is clicked
+    setName("Luigi");
     console.log(name);
   };
 
@@ -12,8 +21,15 @@ const Home = () => {
     <div className="home">
       <h2>Homepage</h2>
 
-      {/* name will not update to Luigi, it stays with Mario */}
-      <p>{name}</p>
+      <p>
+        {
+          // // name will not update to Luigi, it stays with Mario
+          // name
+
+          // since useState is used, name will update to Luigi once button is clicked
+          name
+        }
+      </p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
