79fe9f5bb7bac99befd6a6195c5971727651db28
Leo <leocsdev@gmail.com>
2021-02-28 23:32:18 +0800

    Added useState for loading message and setTimeout to simulate waiting time.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,35 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
   // run json-server to watch db.json in port 8000
   // npx json-server --watch data/db.json --port 8000
-
-  // set initial data from blogs to null
   const [blogs, setBlogs] = useState(null);
 
-  useEffect(() => {
-    // Get blogs from data/db.json
-    fetch("http://localhost:8000/blogs")
-      // convert to json
-      .then((res) => {
-        return res.json();
-      })
-      // set data to blogs state
-      .then((data) => {
-        setBlogs(data);
-      });
+  // Loading message set to true
+  const [isPending, setIsPending] = useState(true);
 
-    // this only runs once since the second arg of useEffect is []
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+
+          // once fetching is done, set isPending to false
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
       {
-        // since fetch is returning a promise, it needs time to load the blog. Only output the BlogList component when fetch is done loading the blogs, otherwise an error will occur since the initial value of blogs is null.
-
-        // Conditional Templating
-        // This will evaluate the left side of && if true,
-        // once left side is true, it will execute the statements in its right side.
-        blogs && <BlogList blogs={blogs} title="All Blogs" />
+        // Show loading message while fetching
+        isPending && <div>Loading...</div>
       }
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
