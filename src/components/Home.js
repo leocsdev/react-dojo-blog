c1a5f43615771e673caf9d80687f7a98870b9ef6
Leo <leocsdev@gmail.com>
2021-02-28 23:22:00 +0800

    Added code to fecth data from db.json with useEffect.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,50 +2,37 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My New Website",
-      body: "Lorem ipsum...",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome Party!",
-      body: "Lorem ipsum...",
-      author: "luigi",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "Lorem ipsum...",
-      author: "princess",
-      id: 3,
-    },
-    {
-      title: "Web dev top tips",
-      body: "Lorem ipsum...",
-      author: "mario",
-      id: 4,
-    },
-  ]);
+  // run json-server to watch db.json in port 8000
+  // npx json-server --watch data/db.json --port 8000
 
-  const [name, setName] = useState("Mario");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  // set initial data from blogs to null
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Use effect ran.");
-    console.log(name);
-  }, [name]);
+    // Get blogs from data/db.json
+    fetch("http://localhost:8000/blogs")
+      // convert to json
+      .then((res) => {
+        return res.json();
+      })
+      // set data to blogs state
+      .then((data) => {
+        setBlogs(data);
+      });
+
+    // this only runs once since the second arg of useEffect is []
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("Luigi")}>change name</button>
-      <p>{name}</p>
+      {
+        // since fetch is returning a promise, it needs time to load the blog. Only output the BlogList component when fetch is done loading the blogs, otherwise an error will occur since the initial value of blogs is null.
+
+        // Conditional Templating
+        // This will evaluate the left side of && if true,
+        // once left side is true, it will execute the statements in its right side.
+        blogs && <BlogList blogs={blogs} title="All Blogs" />
+      }
     </div>
   );
 };
