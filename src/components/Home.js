cd0227e283d7f1e796fa85a2c4077f0a0505b4eb
Leo <leocsdev@gmail.com>
2021-03-01 23:01:32 +0800

    Moved useEffect and state hooks to useFetch custom hook.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,14 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
   // run json-server to watch db.json in port 8000
   // npx json-server --watch data/db.json --port 8000
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not fetch data from blogs resource.");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div className="home">
